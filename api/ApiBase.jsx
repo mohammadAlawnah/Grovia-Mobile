285a3852124d15b18830a15c80761a6986865334
Mohammad Hijjawi <mohammad@goit.ps>
2026-05-06 16:10:20 +0300

    create token and button logout in homeScreen

--- api/ApiBase.jsx
+++ api/ApiBase.jsx
@@ -1,15 +1,15 @@
 import axios from "axios";
+import * as SecureStore from "expo-secure-store";
 
 export const API_URL = "https://grovia-api.vercel.app/api";
 
 const handelErrors = async (err) => {
   if (err?.response?.status === 401) {
-    window.location.href = "/login";
+      console.log("Unauthorized - need login");
   }
 
   if (err?.response?.status === 403) {
     console.log("You don't have permission to access this responce");
-    window.location.href = "/";
   }
 
   return Promise.reject(err);
@@ -21,16 +21,17 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = "token";
-    const tokenType = "Bearer";
+  async (config) => {
+      const token = await SecureStore.getItemAsync("token");
+      const tokenType = "Bearer";
 
-    if (token) {
-      config.headers.Authorization = `${tokenType} ${token}`;
-    }
+      if (token) {
+          config.headers = config.headers || {};
+          config.headers.Authorization = `${tokenType} ${token}`;
+      }
 
-    config.headers["Content-Type"] = "application/json";
-    return config;
+      config.headers["Content-Type"] = "application/json";
+      return config;
   },
   (err) => Promise.reject(err),
 );
